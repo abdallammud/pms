4548d41351041a17d94127bcdeea971b7cdc2fc8
abdallammud <abdallammud@gmai.com>
2025-12-05 19:18:33 +0300

    Changes

--- public/js/script.js
+++ public/js/script.js
@@ -1,19 +1,5 @@
 
 document.addEventListener('DOMContentLoaded', function () {
-    let logoImg = document.querySelector('.logo-img');
-    if (logoImg) {
-        let currentSrc = logoImg.src;
-        let pathParts = currentSrc.split('/');
-        pathParts.pop();
-        let imageBasePath = pathParts.join('/') + '/';
-
-        if (settings.theme === 'light') {
-            logoImg.src = imageBasePath + 'logo.png';
-        } else {
-            logoImg.src = imageBasePath + 'logo-white.png';
-        }
-    }
-
     tinymce.init({
         selector: '.tinymce',
         height: 260
